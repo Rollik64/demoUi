e45add6d1883e7e1dd2f834e4bb0a2ad7b42bb66
Вадим <Rollikak47@gmail.com>
2021-12-27 16:18:18 +0400

    Ok

--- src/main/java/com/example/demoui/HelloApplication.java
+++ src/main/java/com/example/demoui/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-//Пробую ГитХаб
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-    // И тут пробую
+
     public static void main(String[] args) {
         launch();
     }
